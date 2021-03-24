5d40403520913e0a5e41a28e3638bfcb26e997d2
VitaliiUr <vitaliyurbanevich@gmail.com>
2021-03-24 14:26:47 +0100

    import PhaseSpaceFiles130MeV (Simulated in cluster by me)

--- src/PrimaryGeneratorAction.h
+++ src/PrimaryGeneratorAction.h
@@ -17,6 +17,7 @@
 #include <TVector3.h>
 #include <TMath.h>
 #include "DataStorage.h"
+#include "G4SPSRandomGenerator.hh"
 
 namespace SiFi {
 
@@ -35,7 +36,7 @@ class PrimaryGeneratorAction : public G4VUserPrimaryGeneratorAction {
         gps->GetCurrentSource()->GetAngDist()->SetAngDistType("iso");
         gps->GetCurrentSource()->GetEneDist()->SetEnergyDisType("Mono");
         fParticleGun->SetParticleDefinition(particle);
-        fParticleGun->SetParticleEnergy(4400 * keV);
+        // fParticleGun->SetParticleEnergy(4400 * keV);
         fParticleGun->SetParticleMomentumDirection(G4ThreeVector(0,0,1));
         
     };
@@ -50,37 +51,66 @@ class PrimaryGeneratorAction : public G4VUserPrimaryGeneratorAction {
         // }
 
         // }
-        TFile* file = new TFile("PMMA180MeV0mmBPType3_20200603.root", "READ");
-        TTree* ftree = (TTree*)file->Get("Events");
-        int imax;
-        int fEventNumber;
-        Double_t energy;
-        TVector3* pos = 0;
-        G4ThreeVector position;
-        imax = ftree->GetEntries();
-        log->info("imax {}", imax);
-        TVector3* fDir = 0;
         G4ThreeVector direction;
-        ftree->SetBranchAddress("EventNumber", &fEventNumber);
-        ftree->SetBranchAddress("Energy_Primary", &energy);
-        ftree->SetBranchAddress("RealPosition_source", &pos);
-        ftree->SetBranchAddress("RealDirection_source", &fDir);
+        G4ThreeVector position;
+        TFile* file = new TFile("output9.root", "READ");
+        TTree* ftree = (TTree*)file->Get("Secondaries");
+        // int imax;
+        std::vector<double>* energy = 0;
+        std::vector<TVector3>* pos;
+        pos = new std::vector<TVector3>;
+        int imax = ftree->GetEntries();
+        // log->info("imax {}", imax);
+        std::vector<TVector3>* fDir = 0;
+        std::vector<int>* id = 0;
+        std::vector<double>* time = 0;
+        ftree->SetBranchAddress("ParticleID", &id);
+        ftree->SetBranchAddress("ParticleEnergy", &energy);
+        ftree->SetBranchAddress("ParticleTime", &time);
+        ftree->SetBranchAddress("ParticlePosition", &pos);
+        ftree->SetBranchAddress("ParticleMomentum", &fDir);
+
+        G4double rndm, rndm2;
+        G4double px, py, pz;
+        G4double costheta;
+        log->info("imax {}", imax);
         for (int i = 0; i <imax; i++)
         { 
           if (i % 1000000 == 0){
             log->info("{}", i);
           }
-          // std::cout<<"Hello"<<std::endl;
           ftree->GetEntry(i);
-          if ((fDir->X() != 0 || fDir->Y() != 0 || fDir->Z() != 0) && pos->Z()>-150){
-          position = G4ThreeVector(pos->Y(), -pos->Z(), pos->X());
-          direction = G4ThreeVector(fDir->Y(), -fDir->Z(), fDir->X());
-          fParticleGun->SetParticlePosition(position);
-          fParticleGun->SetParticleMomentumDirection(direction);
-          fParticleGun->SetParticleEnergy(energy);
-          fStorage->registerEventStart(event->GetEventID(), position, direction, energy);
-          fParticleGun->GeneratePrimaryVertex(event);
+          for (int j = 0; j < energy->size(); j++)
+          {
+            if (id->at(j) == 22)
+            {
+              position = G4ThreeVector(pos->at(j).Y(), -25.0-pos->at(j).Z(), pos->at(j).X());
+              // position = G4ThreeVector(0,0,0);
+              fParticleGun->SetParticlePosition(position);
+              direction = G4ThreeVector(fDir->at(j).Y(), -fDir->at(j).Z(), fDir->at(j).X());
+              if (direction.theta() < TMath:: Pi()/12 | (2.0 * TMath:: Pi() - direction.theta()) < TMath:: Pi()/12){
+                fParticleGun->SetParticleMomentumDirection(direction);
+                fParticleGun->SetParticleEnergy(energy->at(j));
+                fStorage->registerEventStart(event->GetEventID(), position, direction, energy->at(j));
+                // if(-pos->at(j).Z() < 50.0){
+                fParticleGun->GeneratePrimaryVertex(event);
+                // }
+              }
+
+              break;
+            }
+            
           }
+          
+          // if ((fDir->X() != 0 || fDir->Y() != 0 || fDir->Z() != 0) && pos->Z()>-150){
+          // position = G4ThreeVector(pos->Y(), -pos->Z(), pos->X());
+          // direction = G4ThreeVector(fDir->Y(), -fDir->Z(), fDir->X());
+          // fParticleGun->SetParticlePosition(position);
+          // fParticleGun->SetParticleMomentumDirection(direction);
+          // fParticleGun->SetParticleEnergy(energy);
+          // fStorage->registerEventStart(event->GetEventID(), position, direction, energy);
+          // fParticleGun->GeneratePrimaryVertex(event);
+          // }
         }
         log->info("Done");
         
@@ -91,7 +121,7 @@ class PrimaryGeneratorAction : public G4VUserPrimaryGeneratorAction {
     G4GeneralParticleSource* fSource;
     G4ParticleTable* particleTable = G4ParticleTable::GetParticleTable();
     G4ParticleDefinition* particle = particleTable->FindParticle("gamma");
-    G4ParticleGun* fParticleGun  = new G4ParticleGun(1);
+    G4ParticleGun* fParticleGun  = new G4ParticleGun(10);
     DataStorage* fStorage = nullptr;
 
 };
