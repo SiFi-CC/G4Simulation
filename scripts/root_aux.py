173295da575e1395ea7304ad2ea41be2b5d4404d
VitaliiUr <vitaliyurbanevich@gmail.com>
2021-09-02 15:31:54 +0200

    autoiter

--- scripts/root_aux.py
+++ scripts/root_aux.py
@@ -57,25 +57,30 @@ def get(path, objname):
 
 
 def normalize(x):
-    return (x - x.min())/(x.max() - x.min())
+    if np.unique(x).shape[0] > 1:
+        return (x - x.min())/(x.max() - x.min())
+    else:
+        return np.ones_like(x)
 
 
-def mse_uqi(x, y, normx=True, normy=True):
+def mse_uqi(x, y, normx=False, normy=True):
     if normx:
         x = normalize(x)
     if normy:
         y = normalize(y)
     if x.shape != y.shape:
         raise ValueError("Shapes of arrays are different")
-    mse = np.sqrt(((y - x)**2).mean())
-    # mse = ((y - x)**2).sum()/x.shape[-1]**2
+    # rmse = np.sqrt(((y - x)**2).mean())
+    mse = ((y - x)**2).mean()
     cov = np.cov(x.flatten(), y.flatten())
     uqi = 4*x.mean()*y.mean()*cov[0, 1]/(cov[0, 0]+cov[1, 1])\
         / (x.mean()**2 + y.mean()**2)
-    return mse, uqi, mse/uqi
+    if uqi == 0:
+        uqi = 1e-8
+    return mse, 1/uqi, mse/uqi
 
 
-def mse_uqi_set(x_set, y, normx=True, normy=True):
+def mse_uqi_set(x_set, y, normx=False, normy=True):
     if normy:
         y = normalize(y)
     mse = []
@@ -83,7 +88,6 @@ def mse_uqi_set(x_set, y, normx=True, normy=True):
     comb = []
     for x in x_set:
         mse_tmp, uqi_tmp, comb_tmp = mse_uqi(x, y, normy=False, normx=normx)
-
         mse.append(mse_tmp)
         uqi.append(uqi_tmp)
         comb.append(comb_tmp)
@@ -95,50 +99,23 @@ def reco_mlem(matr, image, niter, reco=None, source=None):
         reco = [np.ones(matr.shape[-1])]
     for _ in tqdm(range(niter), desc="Reconstruction"):
         reco_tmp = reco[-1]*(matr.T @ (image/(matr @ reco[-1])))
-        reco.append(reco_tmp)
+        reco.append(normalize(reco_tmp))
     return reco
 
 
-def recoo_mlem_auto(matr, image, source, reco=None, method="both"):
+def reco_mlem_auto(matr, image, source,
+                   reco=None, method="both", maxiter=2000):
     source_norm = normalize(source)
     if not reco:
         reco = [np.ones(matr.shape[-1])]
-    if len(reco) > 1:
-        score = [mse_uqi(reco[-2], source_norm, normy=False)[SCORE[method]],
-                 mse_uqi(reco[-1], source_norm, normy=False)[SCORE[method]]]
-    else:
-        score = [mse_uqi(reco[-1], source_norm, normy=False)[SCORE[method]]]
-    while len[reco] == 0 or score[-1] > score[-2]:
-        reco_tmp = reco[-1]*(matr.T @ (image/(matr @ reco[-1])))
-        reco.append(normalize(reco_tmp))
-        score.append(mse_uqi(reco[-1], source_norm)[SCORE[method]])
-    return reco, score
-
-
-# class MLEM():
-
-#     def __init__(self, simdata, matrixH, max_iter=2000,
-#                  source=True):
-#         self.max_iter = max_iter
-#         self.system_matrix = get_hmat(matrixH)
-#         if source:
-#             self.simdata, self.source =\
-#                 get_histo(simdata, ["energyDeposits", "sourceHist"])
-#         else:
-#             self.simdata = get_histo(simdata, "energyDeposits")
-#             # TODO test dimensions correspondance
-#         self.source = normalize(self.source)
-#         self.image = [np.ones(self.system_matrix.shape[-1])]
-#         self.current_iter = 0
-#         self.mse = []
-#         self.uqi = []
-
-#     def reco(self, n_iter, score="both"):
-#         if n_iter > 0:
-#             self.image = reco_mlem(self.system_matrix,
-#                                    self.simdata.vals.flatten(),
-#                                    self.n_iter,
-#                                    reco=self.image)
-#         else:
-
-#         self.current_iter = len(self.image)
+    score = mse_uqi_set(reco, source_norm, normy=False)[SCORE[method]]
+    with tqdm(total=maxiter, desc="Reconstruction(autoiter)") as pbar:
+        while len(reco) == 1 or score[-1] < score[-2]:
+            reco_tmp = reco[-1]*(matr.T @ (image/(matr @ reco[-1])))
+            reco.append(normalize(reco_tmp))
+            score.append(mse_uqi(reco[-1], source_norm)[SCORE[method]])
+            pbar.update(1)
+        # pbar.update(maxiter - len(reco) + 1)
+    print(f"{len(reco)-2} iterations was performed")
+    print(f"Best score: {round(score[-2],5)}")
+    return reco[:-1]
